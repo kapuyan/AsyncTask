d9819b97d085371b917536e65124665fdb9d4921
kapuyan <kevin.apuyan@enhanceit.us>
2020-04-25 16:00:37 -0400

    Step 4 Task 2: creating AsyncTask Subclass

---
+++ app/src/main/java/com/example/simpleasynctask/SimpleAsyncTask.kt
@@ -0,0 +1,38 @@
+package com.example.simpleasynctask
+
+import android.os.AsyncTask
+import android.widget.TextView
+import java.lang.ref.WeakReference
+import java.util.*
+
+
+class SimpleAsyncTask: AsyncTask<Void, Void, String>() {
+    private var mTextView: WeakReference<TextView>? = null
+
+
+    fun SimpleAsyncTask(tv: TextView) {
+        mTextView = WeakReference(tv)
+    }
+
+    override fun doInBackground(vararg voids: Void?): String? {
+       //generates a random value from 0 to 10
+        val r = Random()
+        val n: Int = r.nextInt(11)
+
+        //time multiplier, as threads run in ms
+        val s = n * 200
+
+        //sleep for random amount of time
+        try {
+            Thread.sleep(s.toLong())
+        } catch (e: InterruptedException) {
+            e.printStackTrace()
+        }
+        // returns when thread is awake
+        return "Awake at last after sleeping for " + s + " milliseconds!";
+    }
+
+    override fun onPostExecute(result: String?) {
+        mTextView!!.get()!!.text = result
+    }
+}
